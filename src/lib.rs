3d287a76723713a315c251f8acfe7c3a6395de8a
Cem Gurhan <cemg@live.co.uk>
2023-04-05 08:51:05 +0100

    add test and search function

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
-    println!("Searching for query: '{}'\nin path: '{}'", config.query, config.file_path);
-
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("File with text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
@@ -23,5 +23,32 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
         Ok(Config { query, file_path })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result: Vec<&str> = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line)
+        }
+    }
+    result
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
